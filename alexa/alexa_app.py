79ef55952533a55ea5b5bd8e876c8a35a35de536
ishchow <ishaatchowdhury@gmail.com>
2017-01-28 14:25:23 -0700

    Basic launch added

--- alexa/alexa_app.py
+++ alexa/alexa_app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_ask import Ask, statement, question, session
+from flask_ask import Ask, request, statement, question, session
 import json
 import requests
 import time
@@ -12,6 +12,33 @@ ask = Ask(app, "/")
 @app.route('/')
 def homepage():
     return "LET'S WIN HACKED 2017!!!!!"
+
+@ask.launch
+def launch():
+    welcome_message = 'Would you like to play chess?'
+    return question(welcome_message)
+
+@ask.intent("LaunchIntent_Yes")
+def launched():
+    msg = "Okay starting soon...."
+    return statement(msg)
+
+@ask.intent("LaunchIntent_No")
+def fbomb_mlhstye():
+    msg = "Your lame, cool people play chess!!!!"
+    return statement(msg)
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    return statement("Goodbye")
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    return statement("Goodbye")
+
+@ask.session_ended
+def session_ended():
+    return "", 200
 
 if __name__ == "__main__":
     app.run(debug=True)
