de6e0b35afd577580f7a50c2342ceeb8d5a7e98b
cmput274 <kbehaan@gmail.com>
2017-01-29 03:41:22 -0700

    working mostly

--- flasks/startup.py
+++ flasks/startup.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import json
 import sys
 sys.path.append('/home/cmput274/kdehaan/Python/alexa_chess/web')
 import chess as game
+import time
 
 
 app = Flask(__name__)
@@ -11,7 +12,20 @@ chessgame = game.rungame()
 chessboard = game.Board()
 
 
-def update():
+
+@app.route('/', methods=['GET', 'POST'])
+def initialize():
+    return render_template('chess.html')
+
+@app.route('/send/', methods=['POST'])
+def sendDict():
+    s1, s2, d1, d2 = [int(x) for x in raw_input("input: ").split()]
+    chessboard.Move((s1, s2), (d1, d2), chessgame)
+    return jsonify(dict = update(chessboard))
+
+
+def update(chessboard):
+    parsedboard = {}
     for key in chessboard.board:
         if chessboard.board[key] == None:
             parsedboard[str(key)] = ' '
@@ -20,7 +34,7 @@ def update():
             if chessboard.board[key].color == 'white':
                 parsedboard[str(key)] = '&#9817;'
             else:
-                parsedboard[str(key)] = '&#9823;'
+                parsedboard[str(key)] =  '&#9823;'
             continue
         if chessboard.board[key].piecetype == 'king':
             if chessboard.board[key].color == 'white':
@@ -36,7 +50,7 @@ def update():
             continue
         if chessboard.board[key].piecetype == 'bishop':
             if chessboard.board[key].color == 'white':
-                parsedboard[str(key)] = '&#9815;'
+                parsedboard[str(key)] =  '&#9815;'
             else:
                 parsedboard[str(key)] = '&#9821;'
             continue
@@ -48,34 +62,10 @@ def update():
             continue
         if chessboard.board[key].piecetype == 'rook':
             if chessboard.board[key].color == 'white':
-                parsedboard[str(key)] = '&#9814;'
+                parsedboard[str(key)] =  '&#9814;'
             else:
                 parsedboard[str(key)] = '&#9820;'
             continue
-    jsondump = json.dumps(parsedboard)
-    return render_template('chess.html', title='Alexa Chess', testboard=jsondump)
-
-
-
-@app.route('/')
-def initialize():
-    chessboard = game.Board()
-    return update()
-
-@app.route('/waitformove/')
-def waitformove():
-    return update()
-    #source, dest = alexa.Input().split()
-
-@app.route('/checkmove/')
-def checkmove():
-    # chessboard.move(x1, y1, x2, y2, chessgame)
-    return "ended"
-    if chessboard.win():
-        return "someone won"
-    else:
-        return redirect(url_for('waitformove'))
-
-@app.route('/gameover/')
-def restart():
-    return 'ok' #new template
+    return parsedboard
+    # jsondump = json.dumps(parsedboard)
+    # return render_template('chess.html', title='Alexa Chess', testboard=jsondump)
