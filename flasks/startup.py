311090c7c40055354b367135ffcf67e2ea98808d
cmput274 <kbehaan@gmail.com>
2017-01-29 04:28:31 -0700

    almost

--- flasks/startup.py
+++ flasks/startup.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template, request, jsonify
 import json
 import sys
 import os
+
 cwd = os.getcwd()
 cwd = cwd[:-6]
 cwd += 'web/'
-
+print cwd
 sys.path.append(cwd)
 import chess as game
-import time
+
+cwd = cwd[:-4]
+cwd += 'alexa/'
+print cwd
+sys.path.append(cwd)
+import alexa_app
+
 
 
 app = Flask(__name__)
@@ -71,45 +78,6 @@ def update(chessboard):
             else:
                 parsedboard[str(key)] = '&#9820;'
             continue
-<<<<<<< HEAD
-    jsondump = json.dumps(parsedboard)
-<<<<<<< HEAD
-return render_template('chess.html', title='Alexa Chess', testboard=jsondump)
-
-@app.route('/restart/')
-def restart():
-    game.reset()
-    start()
-=======
-    return render_template('chess.html', title='Alexa Chess', testboard=jsondump)
-
-
-
-@app.route('/')
-def initialize():
-    chessboard = game.Board()
-    return update()
-
-@app.route('/waitformove/')
-def waitformove():
-    return update()
-    #source, dest = alexa.Input().split()
-
-@app.route('/checkmove/')
-def checkmove():
-    # chessboard.move(x1, y1, x2, y2, chessgame)
-    return "ended"
-    if chessboard.win():
-        return "someone won"
-    else:
-        return redirect(url_for('waitformove'))
-
-@app.route('/gameover/')
-def restart():
-    return 'ok' #new template
->>>>>>> ce0363a877ce21cf9624bb977c15510a77220ae9
-=======
     return parsedboard
     # jsondump = json.dumps(parsedboard)
     # return render_template('chess.html', title='Alexa Chess', testboard=jsondump)
->>>>>>> de6e0b35afd577580f7a50c2342ceeb8d5a7e98b
