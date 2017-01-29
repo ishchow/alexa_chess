ba879b6b27838bbeabf6c1bf7c572caf0ce7e8dd
LionKingSimba <ttran1@ualberta.ca>
2017-01-29 09:36:47 -0700

    Add files via upload

--- web/chess.py
+++ web/chess.py
@@ -1,3 +1,4 @@
+#newone
 from itertools import product
 
 class Board:
@@ -9,7 +10,9 @@ class Board:
 
     x-axis: a-h
     y-axis: 1-8
+
     """
+
     def __init__(self):
         self.board = {(x,y):None for x in range(8) for y in range(8)}
         self.pieceCount = 32
@@ -17,10 +20,10 @@ class Board:
         self.blackPieces = 16
         self.SpawnAll()
         self.DisplayBoard()
-        self.BKingcanCastleL = True
-        self.WKingcanCastleL = True
-        self.BKingcanCastleR = True
-        self.WKingcanCastleR = True
+        self.whiteKingLoc = (4,0)
+        self.blackKingLoc = (4,7)
+        self.whiteKingCheck = False
+        self.blackKingCheck = False
 
     def SpawnAll(self):
         for x in range(8):
@@ -32,8 +35,8 @@ class Board:
         self.board[(2,0)] = bishop('bishop', 'white')
         self.board[(5,0)] = bishop('bishop', 'white')
         self.board[(4,0)] = king('king', 'white')
-        self.board[(3,0)] = queen('king', 'white')
-        #spawn black pieces
+        self.board[(3,0)] = queen('queen', 'white')
+
         for x in range(8):
               self.board[(x, 6)] = pawn('pawn', 'black')
         self.board[(1, 7)] = knight('knight', 'black')
@@ -45,78 +48,33 @@ class Board:
         self.board[(4,7)] = king('king', 'black')
         self.board[(3,7)] = queen('queen', 'black')
 
+
     def DisplayBoard(self):
         print(self.board)
 
     def GetPieceCount(self):
         print(self.pieceCount)
 
-    def promotion(self, dest, game):
-        piece = self.board[dest]
-        if piece.color == 'black' and dest[1] == 7:
-            print('pawn promotion! choose a piece to promote to\
-            (knight, bishop, rook, queen): ')
-            piece.piecetype = input()
-            return None
-        if piece.color == 'white' and dest[1] == 0:
-            print('pawn promotion! choose a piece to promote to\
-            (knight, bishop, rook, queen): ')
-            piece.piecetype = input()
-            return None
+    """Move Piece
 
-    def blocking(self, potentialmove, loc, dest):
-        #leftright
-        if (potentialmove[0] != 0 and potentialmove[1]) == 0 and (piece.piecetype == ('rook' or 'queen')):
-            left = min(loc[0],dest[0])
-            right = max(loc[0],dest[0])
-            checkblock = [(a,loc[1]) for a in range(left,right)]
-            for i in range(len(checkblock)):
-                if self.board[checkblock[i]] != None:
-                    print('blocked1')
-                    return None
-        #updown
-        print(piece.piecetype)
-        if (potentialmove[0] == 0 and potentialmove[1] != 0) and (piece.piecetype == ('rook' or 'queen')):
-            bot = min(loc[1],dest[1])
-            top = max(loc[1],dest[1])
-            checkblock = [(loc[0],a) for a in range(bot,top)]
-            for i in range(len(checkblock)):
-                if self.board[checkblock[i]] != None:
-                    print('blocked2')
-                    return None
-        #diagonal
-        if (potentialmove[0] == potentialmove[1]) and (piece.piecetype == ('bishop' or 'queen')):
-            botleftx = min(loc[0],dest[0])
-            botlefty = min(loc[1],dest[1])
-            toprightx = max(loc[0],dest[0])
-            toprighty = max(loc[1],dest[1])
-            for j in range(abs(potentialmove[0])):
-                checkblock += [(botleftx+j,botlefty+j)]
-            for i in range(len(checkblock)):
-                if self.board[checkblock[i]] != None:
-                    print('blocked3')
-                    return None
-        if (potentialmove[0] == -(potentialmove[1])) and (piece.piecetype == ('bishop' or 'queen')):
-            topleftx = min(loc[0],dest[0])
-            toplefty = max(loc[1],dest[1])
-            botrightx = max(loc[0],dest[0])
-            botrighty = min(loc[1],dest[1])
-            for j in range(abs(potentialmove[0])):
-                checkblock += [(topleftx+j,toplefty-j)]
-            for i in range(len(checkblock)):
-                if self.board[checkblock[i]] != None:
-                    print('blocked4')
-                    return None
-                    
-    """
     Moves a piece from one location to another
+
     Parameters:
         piece: the chess piece to move
         loc: the current location of the chess piece
         dest: the destination to move to
         game: the game
+
     """
     def Move(self, loc, dest, game):
+        # if game.currentPlayer == "white" and self.whiteKingCheck == True:
+        #     if loc != self.whiteKingLoc:
+        #         print("Please move the white king")
+        #         return None
+        # if game.currentPlayer == "black" and self.blackKingCheck == True:
+        #     if loc != self.blackKingLoc:
+        #         print("Please move the white king")
+        #         return None
         piece = self.board[loc] # the piece object to move
         if piece == None:
             print("There's nobody there, ya dingus!")
@@ -126,62 +84,16 @@ class Board:
             print("he ain't your guy ya dingus!")
             return None
 
+        legalMoves = piece.legalmoves() # a list of all possible movement vectors
+        attemptedMove = (dest[0]-loc[0],dest[1]-loc[1])
+
         if (dest[0] not in range(8) or dest[1] not in range(8)):
             print("That destination ain't on the board, ya dingus!")
             return None
 
-        legalMoves = piece.legalmoves() # a list of all possible movement vectors
-        attemptedMove = (dest[0]-loc[0],dest[1]-loc[1])
-
-        if (piece.piecetype == 'king') and (self.WKingcanCastleL == True):
-            if (attemptedMove == (2,0)) and \
-            ((self.board[(5,0)] and self.board[(6,0)]) == None) and \
-            (self.board[(7,0)] == 'rook'):
-                self.board[dest] = piece
-                self.board[loc] = None
-                self.board[5,0] = 'rook'
-                self.board[7,0] = None
-        if (piece.piecetype == 'king') and (self.WKingcanCastleR == True):
-            if (attemptedMove == (-2,0)) and \
-            ((self.board[(1,0)] and self.board[(2,0)] and self.board[(3,0)]) == None) and \
-            (self.board[(0,0)] == 'rook'):
-                self.board[dest] = piece
-                self.board[loc] = None
-                self.board[3,0] = 'rook'
-                self.board[0,0] = None
-        if (piece.piecetype == 'king') and (self.BKingcanCastleL == True):
-            if (attemptedMove == (2,0)) and \
-            ((self.board[(5,7)] and self.board[(6,7)]) == None) and \
-            (self.board[(7,7)] == 'rook'):
-                self.board[dest] = piece
-                self.board[loc] = None
-                self.board[5,7] = 'rook'
-                self.board[7,7] = None
-        if (piece.piecetype == 'king') and (self.BKingcanCastleR == True):
-            if (attemptedMove == (-2,0)) and \
-            ((self.board[(1,7)] and self.board[(2,7)] and self.board[(3,7)]) == None) and \
-            (self.board[(0,7)] == 'rook'):
-                self.board[dest] = piece
-                self.board[loc] = None
-                self.board[3,7] = 'rook'
-                self.board[0,7] = None
-
-        if piece.piecetype == 'pawn':
-            if (attemptedMove[0] != 0) and (self.board[dest] == None):
-                print('pawns can only move diagonally if an enemy is there')
-                return None
-            if (attemptedMove == ((0,1) or (0,-1))) and (self.board[dest] != None):
-                print('pawns cannot kill things directly in front of them')
-                return None
-            if (attemptedMove == ((0,2) or (0,-2))) and (loc[1] != (1 or 6)):
-                print('pawns can move two spaces only their first move')
-                return None
-
         if attemptedMove not in legalMoves: # If the destination is not possible to move to
-            print("Not a legal move")
+            print("Not a legal destination")
             return None
-
-        blocking(attemptedMove, loc, dest)
 
         if self.board[dest] != None:
             if self.board[dest].color == game.currentPlayer: # If you are moving to a space with your own unit
@@ -195,38 +107,214 @@ class Board:
                 if game.currentPlayer == "white":
                     self.blackPieces -= 1
                     print(self.blackPieces)
-                    game.CurrentPlayer = "black"
+                    game.currentPlayer = "black"
                 else:
                     self.whitePieces -= 1
-                    print(self.whitePieces)
-                    game.CurrentPlayer = "white"
+#                     print(self.whitePieces)
+                    game.currentPlayer = "white"
                 self.board[loc] = None
-                print("It is now %s\'s turn" % (game.currentPlayer))
+                print("Its now %s turn" % (game.currentPlayer))
+                self.SearchForChecks(self.whiteKingLoc, self.blackKingLoc, game.currentPlayer)
+                return None
 
         else:
             print(piece)
             self.board[dest] = piece
             self.board[loc] = None
-            if piece.piecetype == 'king':
-                if piece.color == 'black':
-                    self.BKingcanCastleL = False
-                    self.BKingcanCastleR = False
-                else:
-                    self.WKingcanCastleL = False
-                    self.WKingcanCastleR = False
-            if piece.piecetype == 'rook':
-                if loc == (7,0):
-                    self.WKingcanCastleL = False
-                if loc == (0,0):
-                    self.WKingcanCastleR = False
-                if loc == (7,7):
-                    self.BKingcanCastleL = False
-                if loc == (0,7):
-                    self.BKingcanCastleR = False
-            if piece.piecetype == 'pawn':
-                self.promotion(dest,game)
             game.ChangePlayer()
-            print("It is now %s\'s turn" % (game.currentPlayer))
+            print("Its now %s turn" % (game.currentPlayer))
+            self.SearchForChecks(self.whiteKingLoc, self.blackKingLoc, game.currentPlayer)
+
+
+    def UpdateKingLocation(self, loc, color):
+        """
+        Updates the location of the king
+
+        Parameters:
+            loc - the new location of the king
+            color - the color of the king to move
+
+        """
+
+        if color == "white":
+            self.whiteKingLoc = loc
+        elif color == "black":
+            self.blackKingLoc = loc
+
+    def Check(self, loc, color):
+        """
+        Checks for a checkmate
+
+        Gets called after the player turn change happens.
+
+        Creates a bool check
+        Checks all possible pawn locations for pawns
+        Checks all possible rook locations for rooks and queens
+        Checks all possible bishop locations for bishops and queens
+        Checks all possible knight locations for knights
+
+        Parameters:
+            loc - the location of the king as a tuple
+            color - the current player's turn
+
+        """
+        pawnLocations = []
+        rookLocations = []
+        bishopLocations = []
+        knightLocations = []
+
+        # Get pawn locations
+        if color == "white":
+            pawnLocations = [(loc[0]-1,loc[1]+1),(loc[0]+1,loc[1]+1)]
+        elif color == "black":
+            pawnLocations = [(loc[0]-1,loc[1]-1),(loc[0]+1,loc[1]-1)]
+
+        # Get rook locations
+        rookLocations += [(a,loc[1]) for a in range(8)]
+        rookLocations += [(loc[0],a) for a in range(8)]
+
+        # Get bishop locations
+#         minloc = min(loc[0],loc[1])
+#         maxloc = max(loc[0],loc[1])
+#         botleft = (loc[0]-minloc,loc[1]-minloc)
+#         diag1 = max(botleft[0],botleft[1])
+#         bishopLocations += [(botleft[0]+a,botleft[1]+a) for a in range(7-diag1)]
+#         topleft = (loc[0]-minloc,loc[1]+minloc)
+#         diag2 = max(topleft[0],topleft[1])
+#         bishopLocations += [(botleft[0]+a,botleft[1]-a) for a in range(7-diag2)]
+        bishopLocations += [(loc[0]+a,loc[1]+a) for a in range(-7,8)]
+        bishopLocations += [(loc[0]+a, loc[1]-a) for a in range(-7,8)]
+#         print("Initial bishopLocations: %s" %(bishopLocations))
+        bishopLocations = self.RemoveInvalidSpaces(bishopLocations)
+#         print("After bishopLocations: %s" % (bishopLocations))
+
+        # Get knight locations
+        knightLocations += [(loc[0]+1,loc[1]+2), (loc[0]+1,loc[1]-2)]
+        knightLocations += [(loc[0]-1,loc[1]+2), (loc[0]-1,loc[1]-2)]
+        knightLocations += [(loc[0]+2,loc[1]+1), (loc[0]+2,loc[1]-1)]
+        knightLocations += [(loc[0]-2,loc[1]+1), (loc[0]-2,loc[1]-1)]
+        knightLocations = self.RemoveInvalidSpaces(knightLocations)
+
+        for space in pawnLocations:
+            if (self.board[space] != None): #need this cuz of piecetype stuff don't delete
+                if self.board[space].piecetype == "pawn":
+                    if self.board[space].color != color:
+                        print("pawn check")
+                        return True
+
+        for space in rookLocations:
+            if self.board[space] != None:
+                if ((self.board[space].piecetype == "rook") or (self.board[space].piecetype == "queen")):
+                    if self.board[space].color != color:
+                        if not self.blocking((loc[0]-space[0],loc[1]-space[1]), space, loc):
+                            print("rook/queen check")
+                            return True
+
+        for space in bishopLocations:
+            if self.board[space] != None:
+#                 print('nonecheck')
+#                 print(space)
+#                 print(self.board[space])
+                if ((self.board[space].piecetype == "bishop") or (self.board[space].piecetype == "queen")):
+#                     print('typechk')
+                    if self.board[space].color != color:
+#                         print('colrchk')
+                        if not self.blocking((loc[0]-space[0],loc[1]-space[1]), space, loc):
+                            print("bishop/queen check")
+                            return True
+
+        for space in knightLocations:
+            if (self.board[space] != None):
+                if self.board[space].piecetype == "knight":
+                    if self.board[space].color != color:
+                        print("knight check")
+                        return True
+
+    def RemoveInvalidSpaces(self, spaces):
+        """Removes Invalid Spaces
+
+        Parameters:
+            spaces - a list of spaces
+
+        """
+        filteredSpaces = []
+        for item in spaces:
+#             print(item)
+            if (item[0] > -1) and (item[0] < 8) and (item[1] > -1) and (item[1] < 8):
+                filteredSpaces.append(item)
+#         print(filteredSpaces)
+        return filteredSpaces
+
+    def SearchForChecks(self, whiteloc, blackloc, color):
+        if color == "white": #white turn see if white is in checkmate
+            self.whiteKingCheck = self.Check(whiteloc, color)
+            if self.whiteKingCheck:
+                print("white king in check")
+        elif color == "black":
+#             print('testingtesting123')
+            self.blackKingCheck = self.Check(blackloc, color)
+#             print('testingtesting321')
+            if self.blackKingCheck:
+                print("black king in check")
+
+    def CheckForSafeMoves(self, legalMoves): #returns false if there are no safe moves
+        safeMoves = []
+        safeMoves.append(legalMoves)
+        for item in safeMoves:
+            if (item[0] not in range(8) or item[1] not in range(8)):
+                item = None
+            elif self.board[item] != None:
+                if self.board[item].color == game.currentPlayer:
+                    item = None
+        if not safeMoves:
+            return False
+
+    def blocking(self, potentialmove, loc, dest):
+        #leftright
+        if (potentialmove[0] != 0 and potentialmove[1]) == 0 and (self.board[loc].piecetype == ('rook' or 'queen')):
+            left = min(loc[0],dest[0])
+            right = max(loc[0],dest[0])
+            checkblock = [(a,loc[1]) for a in range(left,right)]
+            for i in range(len(checkblock)):
+                if self.board[checkblock[i]] != None:
+                    print('blocked1')
+                    return True
+        #updown
+        # print(piece.piecetype)
+        if (potentialmove[0] == 0 and potentialmove[1] != 0) and (self.board[loc].piecetype == ('rook' or 'queen')):
+            bot = min(loc[1],dest[1])
+            top = max(loc[1],dest[1])
+            checkblock = [(loc[0],a) for a in range(bot,top)]
+            for i in range(len(checkblock)):
+                if self.board[checkblock[i]] != None:
+                    print('blocked2')
+                    return True
+        #diagonal
+        if (potentialmove[0] == potentialmove[1]) and (self.board[loc].piecetype == ('bishop' or 'queen')):
+            botleftx = min(loc[0],dest[0])
+            botlefty = min(loc[1],dest[1])
+            toprightx = max(loc[0],dest[0])
+            toprighty = max(loc[1],dest[1])
+            for j in range(abs(potentialmove[0])):
+                checkblock += [(botleftx+j,botlefty+j)]
+            for i in range(len(checkblock)):
+                if self.board[checkblock[i]] != None:
+                    print('blocked3')
+                    return True
+        if (potentialmove[0] == -(potentialmove[1])) and (self.board[loc].piecetype == ('bishop' or 'queen')):
+            topleftx = min(loc[0],dest[0])
+            toplefty = max(loc[1],dest[1])
+            botrightx = max(loc[0],dest[0])
+            botrighty = min(loc[1],dest[1])
+            for j in range(abs(potentialmove[0])):
+                checkblock += [(topleftx+j,toplefty-j)]
+            for i in range(len(checkblock)):
+                if self.board[checkblock[i]] != None:
+                    print('blocked4')
+                    return True
+        else:
+            return False
+
 
 
 class chesspiece(object):
@@ -243,7 +331,6 @@ class chesspiece(object):
         self.color = color
 
 class king(chesspiece):
-    # king.piecetype = 'king'
     def legalmoves():
         kingmoves = [(a,b) for a,b in product(range(-1,2),repeat = 2)]
         kingmoves.remove((0,0))
@@ -251,37 +338,31 @@ class king(chesspiece):
         return kingmoves+castlemove
 
 class queen(chesspiece):
-    # queen.piecetype = 'queen'
     def legalmoves(self):
-        return horizontal+vertical+diagonal1+diagonal2
+        return self.horizontal+self.vertical+self.diagonal1+self.diagonal2
 
 class bishop(chesspiece):
-    # bishop.piecetype = 'bishop'
     def legalmoves(self):
-        return diagonal1+diagonal2
+        return self.diagonal1+self.diagonal2
 
 class knight(chesspiece):
-    # knight.piecetype = 'knight'
     def legalmoves(self):
         return [(-1,2), (1,2), (2,1), (2,-1), (-2,1), (-2,1), (1,-2), (-1,-2)]
 
 class rook(chesspiece):
-    # rook.piecetype = 'rook'
     def legalmoves(self):
         return self.horizontal+self.vertical
 
 class pawn(chesspiece):
-    # pawn.piecetype = 'pawn'
     def legalmoves(self):
         if self.color == "white":
             return [(0,1),(0,2),(-1,1),(1,1)]
         elif self.color == "black":
             return [(0,-1),(0,-2),(-1,-1),(1,-1)]
 
-
 class rungame():
     def __init__(self):
-        self.gameboardinstance = Board()
+        self.gameBoard = Board()
         self.currentPlayer = "white"
 
     def GetCurrentPlayer(self):
@@ -294,6 +375,7 @@ class rungame():
             self.currentPlayer = "white"
         else:
             print("I don't know who's turn it is")
+
 
     def getinput(self):
         print('enter a location and destination as tuples:')
