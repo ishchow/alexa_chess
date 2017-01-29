a70d1df18a21168169a0fad2c7ae67848a49e313
cmput274 <kbehaan@gmail.com>
2017-01-28 21:53:13 -0700

    board working

--- flasks/startup.py
+++ flasks/startup.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 import json
-import web.chess as game
+import classdef as game
 
 
 
@@ -10,7 +10,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
+    parsedboard = {}
     gameboard = game.Board()
-
-    jsond = json.dumps(gameboard)
+    for key in gameboard.board:
+        if gameboard.board[key] == None:
+            parsedboard[str(key)] = ' '
+            continue
+        if gameboard.board[key].piecetype == 'pawn':
+            if gameboard.board[key].color == 'white':
+                parsedboard[str(key)] = '&#9817;'
+            else:
+                parsedboard[str(key)] = '&#9823;'
+            continue
+        if gameboard.board[key].piecetype == 'king':
+            parsedboard[str(key)] = 'k'
+            continue
+        if gameboard.board[key].piecetype == 'queen':
+            parsedboard[str(key)] = 'q'
+            continue
+        if gameboard.board[key].piecetype == 'bishop':
+            parsedboard[str(key)] = 'b'
+            continue
+        if gameboard.board[key].piecetype == 'knight':
+            parsedboard[str(key)] = 'n'
+            continue
+        if gameboard.board[key].piecetype == 'rook':
+            parsedboard[str(key)] = 'r'
+            continue
+    jsond = json.dumps(parsedboard)
     return render_template('chess.html', title='Alexa Chess', testboard=jsond)
